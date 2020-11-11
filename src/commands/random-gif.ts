6cf7fe2f8d3a056a79b2aaeafa2d003e0278e5e1
Bruno Mello <bruno.barros.mello@gmail.com>
2020-11-11 19:25:11 -0300

    feat: finish random gif command

--- src/commands/random-gif.ts
+++ src/commands/random-gif.ts
@@ -1,6 +1,11 @@
 import Discord from 'discord.js';
+import giphy from '../services/giphy';
 
-export function randomGif(message: Discord.Message) {
+export async function randomGif(message: Discord.Message): Promise<void> {
   const word = message.content.split(' ')[1];
   if (!word) return;
+
+  const gifUrl = await giphy.searchGifByWord(word);
+
+  await message.reply(gifUrl);
 }
